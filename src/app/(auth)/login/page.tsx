560bef62ba7e69af2a8c3355581f01f886ea5c67
dwigunardi <dwigunardi@gmail.com>
2025-08-22 02:09:21 +0700

    feat(rpos-002): initial setup form validation with zod

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from "./_components/login"
+
+export const metadata = {
+    title: "Kumpul Cafe | Login",
+    description: "Login to your account",
+}
+
+export default function LoginPage() {
+    return <Login />
+}
